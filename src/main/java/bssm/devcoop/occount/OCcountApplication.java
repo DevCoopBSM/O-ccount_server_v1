3be1cb894aa62a504c49ea0f61f12794d7bbc99f
wwwxsv19 <wwwxsv19@gmail.com>
2024-04-12 11:00:41 +0900

    set : project

---
+++ src/main/java/bssm/devcoop/occount/OCcountApplication.java
@@ -0,0 +1,13 @@
+package bssm.devcoop.occount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OCcountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OCcountApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bssm/devcoop/occount/OCcountApplicationTests.java
@@ -0,0 +1,13 @@
+package bssm.devcoop.occount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OCcountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
